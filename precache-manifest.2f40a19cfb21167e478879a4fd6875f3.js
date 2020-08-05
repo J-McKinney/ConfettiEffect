d78632f8920e105924a2da081a275c82b0426589
Jesse McKinney <jessemckinney513@gmail.com>
2020-08-05 15:41:44 -0400

    Updates

---
+++ precache-manifest.2f40a19cfb21167e478879a4fd6875f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "257d6ca5459174e7d0182cedab2cc972",
+    "url": "/ConfettiEffect/index.html"
+  },
+  {
+    "revision": "745340539fc4a0072e4a",
+    "url": "/ConfettiEffect/static/css/main.8fed40f6.chunk.css"
+  },
+  {
+    "revision": "89142d62dd9e83421696",
+    "url": "/ConfettiEffect/static/js/2.dadaacae.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/ConfettiEffect/static/js/2.dadaacae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "745340539fc4a0072e4a",
+    "url": "/ConfettiEffect/static/js/main.e8c2b412.chunk.js"
+  },
+  {
+    "revision": "15d4d21bf81225890d8d",
+    "url": "/ConfettiEffect/static/js/runtime-main.52f1a260.js"
+  }
+]);
